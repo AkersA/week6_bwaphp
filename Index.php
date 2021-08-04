d261146bf89e05b523c2436f20203f6aa6ca2202
Aaron Akers <aja.2013@yahoo.com>
2021-08-04 11:00:22 -0400

    start
    Started html part

--- Index.php
+++ Index.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 
 <html>
-<head><\head>
-<body><\body>
-<\html>
+<head>
+    <meta charset="utf-8">
+    <title>md5 Aaron Akers</title>
+</head>
+<body>
+    <h1>Week 7: md5 assignment</h1>
+    <form method="get">
+        <label for="userEntry">Enter md5:</label>
+        <input type="text" id="userEntry">
+        <input type="submit">
+    </form>
+
+</body>
+</html>
